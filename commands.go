5c7b5051c4bff7a2b82e546cd6c91063ad78d031
Brian Tomlinson <brian@smartm.no>
2015-01-22 18:16:29 +0100

    split commands out to their own file

---
+++ commands.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"github.com/darthlukan/cakeday"
+	"github.com/darthlukan/goduckgo/goduckgo"
+)
+
+// GenericVerbCmd returns a message string based on the supplied cmd (a verb).
+func GenericVerbCmd(cmd, extra string) string {
+	// This should give us something like:
+	//     "Snuffles slaps $USER, FOR SCIENCE!"
+	// If given the command:
+	//     "!slap $USER"
+	randQuip := RandomQuip()
+	return fmt.Sprintf("\x01"+"ACTION %v %v, %v\x01", cmd, extra, randQuip)
+}
+
+// CakeDayCmd returns a string containing the Reddit cakeday of a user
+// upon success, or an error string on failure.
+func CakeDayCmd(user string) string {
+	var msg string
+	// !cakeday $USER
+	responseString, err := cakeday.Get(user)
+	if err != nil {
+		msg = fmt.Sprintf("I caught an error: %v\n", err)
+	} else {
+		// >> Reddit Cake Day for $USER is: $CAKEDAY
+		msg = fmt.Sprintf("%v\n", responseString)
+	}
+	return msg
+}
+
+// WebSearch takes a query string as an argument and returns
+// a formatted string containing the results from DuckDuckGo.
+func SearchCmd(query string) string {
+	msg, err := goduckgo.Query(query)
+	if err != nil {
+		return fmt.Sprintf("DDG Error: %v\n", err)
+	}
+
+	switch {
+	case len(msg.RelatedTopics) > 0:
+		return fmt.Sprintf("First Topical Result: [ %s ]( %s )\n", msg.RelatedTopics[0].FirstURL, msg.RelatedTopics[0].Text)
+	case len(msg.Results) > 0:
+		return fmt.Sprintf("First External result: [ %s ]( %s )\n", msg.Results[0].FirstURL, msg.Results[0].Text)
+	case len(msg.Redirect) > 0:
+		return fmt.Sprintf("Redirect result: %s\n", UrlTitle(msg.Redirect))
+	default:
+		return fmt.Sprintf("Query: '%s' returned no results.\n", query)
+	}
+}
+
+func HelpCmd() string {
+	return fmt.Sprintf("Available commands: !help, !ddg/search !weather (NYI), !cakeday, !VERB\n")
+}
+
+func WikiCmd(config *Config) string {
+	return fmt.Sprintf("(Channel Wiki)[ %s ]\n", config.WikiLink)
+}
+
+func HomePageCmd(config *Config) string {
+	return fmt.Sprintf("(Channel Homepage)[ %s ]\n", config.Homepage)
+}
+
+func ForumCmd(config *Config) string {
+	return fmt.Sprintf("(Channel Forums)[ %s ]\n", config.Forums)
+}
