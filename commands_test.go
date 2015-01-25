4c35bc0b1b13abc2ab90ac608bdfdeb4fd2fb772
Brian Tomlinson <brian@smartm.no>
2015-01-25 13:54:54 +0100

    more tests, these pass locally

--- commands_test.go
+++ commands_test.go
@@ -18,3 +18,52 @@ func TestCakeDayCmd(t *testing.T) {
 		t.Errorf("CakeDayCmd(%v), got %v, want 'Reddit cakeday for darthlukan is: 9 June 2010\n", "darthlukan", cakedayMsg)
 	}
 }
+
+func TestHelpCmd(t *testing.T) {
+	helpMsg := HelpCmd()
+	if !strings.Contains(helpMsg, "Available commands: !help, !ddg/search") {
+		t.Errorf("HelpCmd(), got %v, want 'Available commands: !help, !ddg/search'\n", helpMsg)
+	}
+}
+
+func TestWikiCmd(t *testing.T) {
+	config := &Config{}
+	config.WikiLink = "WIKIURL"
+	wikiUrl := WikiCmd(config)
+	if !strings.Contains(wikiUrl, "WIKIURL") {
+		t.Errorf("WikiCmd(%v), got %v, want 'WikiUrl'\n", config, wikiUrl)
+	}
+}
+
+func TestHomePageCmd(t *testing.T) {
+	config := &Config{}
+	config.Homepage = "HOMEURL"
+	homeUrl := HomePageCmd(config)
+	if !strings.Contains(homeUrl, "HOMEURL") {
+		t.Errorf("HomePageCmd(%v), got %v, want 'HOMEURL'\n", config, homeUrl)
+	}
+}
+
+func TestForumCmd(t *testing.T) {
+	config := &Config{}
+	config.Forums = "FORUMURL"
+	forumUrl := ForumCmd(config)
+	if !strings.Contains(forumUrl, "FORUMURL") {
+		t.Errorf("ForumCmd(%v), got %v, want 'FORUMURL'\n", config, forumUrl)
+	}
+}
+
+func TestSearchCmd(t *testing.T) {
+	topicalResult := SearchCmd("New York")
+	if !strings.Contains(topicalResult, "First Topical Result:") {
+		t.Errorf("SearchCmd(%v), got %v, want 'First Topical Result:'\n", "New York", topicalResult)
+	}
+	redirectResult := SearchCmd("!archwiki i3")
+	if !strings.Contains(redirectResult, "Redirect result:") {
+		t.Errorf("SearchCmd(%v), got %v, want 'Redirect result:'\n", "!archwiki i3", redirectResult)
+	}
+	noResult := SearchCmd("my face")
+	if !strings.Contains(noResult, "returned no results") {
+		t.Errorf("SearchCmd(%v), got %v, want 'Query: my face returned no results.'\n", "your face", noResult)
+	}
+}
